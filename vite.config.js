28a4a6ad224bd9d980fe3cb85ea9a5d36af1b04a
Ankita Yadav <ankitayadav476076@gmail.com>
2026-01-29 23:03:00 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
+=======
+  base:'/portfolio/',
+  base:"/Quiz-App",
+>>>>>>> 15c4625 (first commit)
   plugins: [react()],
 })
