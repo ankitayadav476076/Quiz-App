cf1f8f2e97bc1f2ff3a89fb4f3c77cb4c5a6e1aa
Ankita Yadav <ankitayadav476076@gmail.com>
2026-03-03 11:38:30 +0530

    vercel fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Quiz-App/", // Use this if you deploy on GitHub Pages under "Quiz-App" repo
+  base: "/", // Use this if you deploy on GitHub Pages under "Quiz-App" repo
   plugins: [react()],
 });
